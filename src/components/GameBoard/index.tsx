4b1914bbf6832bf8d1f3fda1f551746acaf20920
Frederico Miranda <frederico.miranda.brandao@gmail.com>
2021-08-07 09:51:09 -0400

    Refactor 'GameButton' into its own component.

--- src/components/GameBoard/index.tsx
+++ src/components/GameBoard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import GameButton from '../../components/GameButton/index';
 import ReversiGame from '../../models/ReversiGame';
 
 import './index.css';
@@ -11,52 +12,6 @@ PIECE_NAMES.set(ReversiGame.Piece.NONE, "none");
 PIECE_NAMES.set(ReversiGame.Piece.BLACK, "black");
 PIECE_NAMES.set(ReversiGame.Piece.WHITE, "white");
 
-interface PropsGameButton {
-  styleBackLayer: object;
-  styleBlack: object;
-  styleWhite: object;
-  click: (x: number, y: number) => void;
-  x: number;
-  y: number;
-  piece: string;
-}
-
-const GameButton: React.FC<PropsGameButton> = (props) => {
-
-  const { styleBackLayer, styleBlack, styleWhite } = props;
-
-  const { click, x, y } = props;
-
-  const { piece } = props;
-
-  const propsButton = {
-    style: styleBackLayer,
-    onClick: () => click(x, y),
-    'data-piece': piece,
-  };
-
-  const propsBackLayer = {
-    style: styleBackLayer,
-  };
-
-  const propsBlack = {
-    style: styleBlack,
-  };
-
-  const propsWhite = {
-    style: styleWhite,
-  };
-
-  return (
-    <button className="GameButton" { ...propsButton }>
-      <div className="GameButton-back-layer" { ...propsBackLayer }></div>
-      <div className="GameButton-piece-black" { ...propsBlack }></div>
-      <div className="GameButton-piece-white" { ...propsWhite }></div>
-    </button>
-  );
-
-};
-
 interface PropsGameBoard {
   color: string;
   board: ReversiGame;
@@ -68,43 +23,7 @@ interface PropsGameBoard {
 
 const GameBoard: React.FC<PropsGameBoard> = (props) => {
 
-  const [styleBackLayer, setStyleBackLayer] = React.useState<object>({});
-
-  const [styleBlack, setStyleBlack] = React.useState<object>({});
-
-  const [styleWhite, setStyleWhite] = React.useState<object>({});
-
   const { board, moves, click, color, iconBlack, iconWhite } = props;
-
-  React.useEffect(() => {
-
-    const styleBackLayer = Object.freeze({
-      background: color,
-    });
-
-    setStyleBackLayer(styleBackLayer);
-
-  }, [color]);
-
-  React.useEffect(() => {
-
-    const styleBlack = Object.freeze({
-      backgroundImage: `url(${ iconBlack })`,
-    });
-
-    setStyleBlack(styleBlack);
-
-  }, [iconBlack]);
-
-  React.useEffect(() => {
-
-    const styleWhite = Object.freeze({
-      backgroundImage: `url(${ iconWhite })`,
-    });
-
-    setStyleWhite(styleWhite);
-
-  }, [iconWhite]);
 
   const pieces = [];
 
@@ -116,10 +35,10 @@ const GameBoard: React.FC<PropsGameBoard> = (props) => {
 
       const piece: string = PIECE_NAMES.get(board.getPiece(x, y)) as string;
 
-      const propsGameButton: PropsGameButton = {
-        styleBackLayer: styleBackLayer,
-        styleBlack: styleBlack,
-        styleWhite: styleWhite,
+      const propsGameButton = {
+        iconBlack: iconBlack,
+        iconWhite: iconWhite,
+        color: color,
         click: click,
         piece: piece,
         x: x,
---
+++ src/components/GameButton/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import ReversiGame from '../../models/ReversiGame';
+
+import './index.css';
+
+const PIECE_NAMES = new Map<number, string>();
+
+PIECE_NAMES.set(ReversiGame.Piece.NONE, "none");
+PIECE_NAMES.set(ReversiGame.Piece.BLACK, "black");
+PIECE_NAMES.set(ReversiGame.Piece.WHITE, "white");
+
+interface PropsGameButton {
+  iconBlack: string;
+  iconWhite: string;
+  color: string;
+  click: (x: number, y: number) => void;
+  x: number;
+  y: number;
+  piece: string;
+}
+
+const GameButton: React.FC<PropsGameButton> = (props) => {
+
+  const { color, iconBlack, iconWhite } = props;
+
+  const { click, x, y } = props;
+
+  const { piece } = props;
+
+  const styleBackLayer = Object.freeze({
+    background: color,
+  });
+
+  const styleBlack = Object.freeze({
+    backgroundImage: `url(${ iconBlack })`,
+  });
+
+  const styleWhite = Object.freeze({
+    backgroundImage: `url(${ iconWhite })`,
+  });
+
+  const propsButton = {
+    onClick: () => click(x, y),
+    'data-piece': piece,
+  };
+
+  const propsBackLayer = {
+    style: styleBackLayer,
+  };
+
+  const propsBlack = {
+    style: styleBlack,
+  };
+
+  const propsWhite = {
+    style: styleWhite,
+  };
+
+  return (
+    <button className="GameButton" { ...propsButton }>
+      <div className="GameButton-back-layer" { ...propsBackLayer }></div>
+      <div className="GameButton-piece-black" { ...propsBlack }></div>
+      <div className="GameButton-piece-white" { ...propsWhite }></div>
+    </button>
+  );
+
+};
+
+export default GameButton;
